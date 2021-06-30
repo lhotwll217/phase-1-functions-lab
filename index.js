af3439b67a3d8c9c2ccbc5892c0c94e6b9415de8
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-29 21:23:25 -0400

    commit tree 399bb40b1baee927bf25bcf86d336c369bcc411b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let distanceInBlocks = Math.abs(blocks - 42);
+    return distanceInBlocks
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+
+}
+
+function distanceTravelledInFeet(x, y) {
+    return Math.abs(x - y) * 264
+}
+
+function calculatesFarePrice(x, y) {
+    let fareFeet = distanceTravelledInFeet(x, y);
+    if (fareFeet <= 400) {
+        return 0
+    }
+    else if (fareFeet > 400 && fareFeet < 2000) {
+        return (fareFeet - 400) * .02
+    }
+    else if (fareFeet > 2000 && fareFeet < 2500) {
+        return 25
+    }
+    else if (fareFeet > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
